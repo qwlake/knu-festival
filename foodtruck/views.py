84d185f5764cdabe5e4e150b308f5be88e249d7e
unknown <ksy201811677@likelion.org>
2019-09-05 23:29:16 +0900

    이동 버튼, if문 통해서 divi값으로 푸드트럭 구분

--- foodtruck/views.py
+++ foodtruck/views.py
@@ -16,13 +16,9 @@ def foodtruck(request):
 
 def haminseop(request):
     foodtrucks = Foodtruck.objects.all()
-    foodtrucks1 = foodtrucks[:int(len(foodtrucks)/2+1)]
-    foodtrucks2 = foodtrucks[int(len(foodtrucks)/2+1):]
     booths = Booth.objects.all()
-    booths1 = booths[:int(len(booths)/2+1)]
-    booths2 = booths[int(len(booths)/2+1):]
     # print(foodtrucks.name)
-    return render(request,'haminseop.html',{'foodtrucks1': foodtrucks1, 'foodtrucks2': foodtrucks2, 'booths1': booths1, 'booths2': booths2})
+    return render(request,'haminseop.html',{'foodtrucks': foodtrucks, 'booths': booths})
 
 def mirae(request):
     booths = Booth.objects.all()
