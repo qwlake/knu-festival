e6270a8f5346618698c13034d32d82c60a2d8d54
DONGWON KIM <lunacircle4@likelion.org>
2019-09-07 02:50:01 +0900

    feat: release test

--- festival/settings/production.py
+++ festival/settings/production.py
@@ -30,6 +30,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
