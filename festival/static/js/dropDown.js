3d67b5b3ee0dc742ce62db73d991131d4a7414dc
chap95 <chap95@likelion.org>
2019-09-06 18:09:21 +0900

    fix: dropDown

--- festival/static/js/dropDown.js
+++ festival/static/js/dropDown.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
+    $(".item1").hide()
     var state = 1;
     $(".dropMenu").click(function (){
         if (state == 1){
             $(this).parent().find(".subNav").slideDown('normal').show();
+            $(".item1").show()
             state = state + 1;
         }else{
             $(this).parent().find(".subNav").slideUp('fast');
