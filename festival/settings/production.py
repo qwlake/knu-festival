e697399d9ad558e001c164fce6560a6b63128622
DONGWON KIM <lunacircle4@likelion.org>
2019-09-07 02:01:56 +0900

    feat: release test

--- festival/settings/production.py
+++ festival/settings/production.py
@@ -13,8 +13,12 @@ ALLOWED_HOSTS = ['knu-festival-test-env.3wfmyausyg.ap-northeast-2.elasticbeansta
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
 }
 
