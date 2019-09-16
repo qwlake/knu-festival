5bd355d93875d3f62f81b74c5e056944572dedee
park <pannchat@likelion.org>
2019-09-16 10:19:05 +0000

    base

--- festival/settings/development.py
+++ festival/settings/development.py
@@ -13,8 +13,12 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': 'myfestivaldb.cewp4aaqoxuh.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+        'NAME': 'deploy',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
 }
 
